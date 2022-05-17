687db7fba27a30c6c0e2f9d24da2db642715ee38
vmatyusGitHub <veronika.matyus@gmail.com>
2022-05-17 15:35:14 +0200

    Update tag handling only in BeforeScenario hook

--- messageprocessors/ScenarioExecutionStartingRequestProcessor.go
+++ messageprocessors/ScenarioExecutionStartingRequestProcessor.go
@@ -9,6 +9,11 @@ type ScenarioExecutionStartingRequestProcessor struct{}
 
 func (r *ScenarioExecutionStartingRequestProcessor) Process(msg *m.Message, context *t.GaugeContext) *m.Message {
 	tags := msg.GetScenarioExecutionStartingRequest().GetCurrentExecutionInfo().GetCurrentScenario().GetTags()
+	fmt.Println("==> Current Scenario Tags:", tags)
+	fmt.Println("==> Current Spec Tags:", msg.GetScenarioExecutionStartingRequest().GetCurrentExecutionInfo().GetCurrentSpec().GetTags())
+	specTags := msg.GetScenarioExecutionStartingRequest().GetCurrentExecutionInfo().GetCurrentSpec().GetTags()
+	tags = append(tags, specTags[:]...)
+	fmt.Println("==> Merged Tags:", tags)
 	hooks := context.GetHooks(t.BEFORESCENARIO, tags)
 	exInfo := msg.GetScenarioExecutionStartingRequest().GetCurrentExecutionInfo()
 
