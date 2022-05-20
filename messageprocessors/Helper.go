d3b18a73da7ebcf889cfee5c77f751154be421d4
Veronika Matyus <veronika.matyus@ibm.com>
2022-05-20 17:09:28 +0200

    Some awesome title of about 50 chars
     Issue:
    
     A description that is wrapped about 72 chars so shows up
     decently in `git log`.

--- messageprocessors/Helper.go
+++ messageprocessors/Helper.go
@@ -5,8 +5,6 @@ import (
 	t "github.com/getgauge-contrib/gauge-go/testsuit"
 )
 
-// Some note
-
 func executeHooks(hooks []t.Hook, msg *m.Message, exInfo *m.ExecutionInfo) *m.Message {
 	var res *m.ProtoExecutionResult
 	var totalExecutionTime int64
